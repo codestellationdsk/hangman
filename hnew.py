a544b48e29bf005c76bf97201b3bff52a7450428
codestellationdsk <90565235+codestellationdsk@users.noreply.github.com>
2021-09-28 09:55:06 +0530

    Add files via upload

---
+++ hnew.py
@@ -0,0 +1,105 @@
+import random
+def hangman():
+
+     words_list = ["elephant","rabbit","lion","tiger","zebra","giraffe","panther","donkey","monkey","deer","gorilla","chimpanzee"]
+     word = random.choice(words_list)
+     turns=10
+     guessed= ''
+     valid_entry = set('abcdefghijklmnopqrstuvwxyz')
+
+     while len(word)>0:
+         correctWord=""   
+         
+         for letter in word: 
+              if letter in guessed:
+                 correctWord = correctWord + letter
+              else:
+                 correctWord = correctWord + "_ "
+               
+         if correctWord == word:
+            print(correctWord)
+            print("Congratulations!!! ***You Won!***")  
+            print("The word is" , word)        
+            break
+
+         print ("Guess the word",correctWord)
+         guess = input()  
+    
+         if guess in valid_entry:
+             guessed = guessed + guess
+         else:
+             print("Enter a valid character")
+             guess = input()
+         if guess not in word:
+             turns = turns-1
+    
+             if turns == 9:
+                print("You have 9 turns left")
+                print("---------")
+                print("      O  ")
+             if turns == 8:
+                print("You have 8 turns left")
+                print("---------")
+                print("      O  ")
+                print("      |  ")
+             if turns == 7:
+                print("You have 7 turns left")
+                print("---------")
+                print("      O  ")
+                print("      |  ")
+                print("      |  ")
+             if turns == 6:
+                print("You have 6 turns left")
+                print("---------")
+                print("      O  ")
+                print("      |/ ")
+                print("      |  ")
+             if turns == 5:
+                print("You have 5 turns left")
+                print("---------")
+                print("      O  ")
+                print("     \|/ ")
+                print("      |  ")
+             if turns == 4:
+                print("You have 4 turns left")
+                print("---------")
+                print("      O  ")
+                print("     \|/ ")
+                print("      |  ")
+                print("       \ ")
+             if turns == 3:
+                print("You have 3 turns left")
+                print("---------")
+                print("      O  ")
+                print("     \|/ ")
+                print("      |  ")
+                print("     / \ ")
+             if turns == 2:
+                print("You have 2 turns left")
+                print("---------")
+                print("      O_ ")
+                print("     \|/ ")
+                print("      |  ")
+                print("     / \ ")
+             if turns == 1:
+                print("You have 1 turns left")
+                print("---------")
+                print("      O_|")
+                print("     \|/ ")
+                print("      |  ")
+                print("     / \ ")
+             if turns == 0 :
+                print("You loose")
+                print("---------")
+                print("      O_|")
+                print("     /|\ ")
+                print("      |  ")
+                print("     / \ ")
+                print("The correct word was",word)
+                break
+         
+              
+name= input("Enter your name: ")
+print("Welcome",name,"!!")
+print("Good luck, try to find the correct word")
+hangman()
